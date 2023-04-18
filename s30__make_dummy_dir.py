aa0dba25679670d131a2a60331f6a0ebbbc2cf3c
Yu Xin <scaner@gmail.com>
2023-04-18 22:27:11 +0800

    add script for creating dummy dir.

---
+++ s30__make_dummy_dir.py
@@ -0,0 +1,47 @@
+import csv
+import os
+
+SOURCE_FN = 'v10_0.csv'
+DRY = False
+# kid, ksn, kcn, ken
+# id, sn, cn, en
+DIR_PATTERN = '{kcn}__{ken}/{cn}__{en}'
+DIR_PATTERN = '{kcn}/{cn}'
+
+def main():
+    cur_family = {}
+    with open(SOURCE_FN) as fi:
+        ci = csv.reader(fi)
+        data = [i for i in ci]
+    
+    res = []
+    for row in data:
+        if row[4] == '':
+            cur_family = {
+                'kid': row[0],
+                'ksn': row[1],
+                'kcn': row[2],
+                'ken': row[3].partition(' (')[0]
+            }
+            continue
+        
+        species = dict(cur_family)
+        species.update({
+            'id': row[0],
+            'sn': row[1].replace(' ', '_'),
+            'cn': row[2].replace(' ', '_'),
+            'en': row[3].replace(' ', '_'),
+            'iucn': row[4],
+            'level': row[5],
+        })
+        if DRY:
+            print('mkdir -p ' + DIR_PATTERN.format(**species))
+        else:
+            dir_name = "birds/" + DIR_PATTERN.format(**species)
+            os.makedirs(dir_name, exist_ok=True)
+            with open(f'{dir_name}/__KEEP', 'w') as fo:
+                pass
+
+
+if __name__ == '__main__':
+    main()
